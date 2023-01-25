90b9b3fd9770c55cb8441dfc581d4a6de6743756
yskandre <54952885+yskandre@users.noreply.github.com>
2023-01-25 05:23:45 +0100

    Bugfixes

--- Assets/Scripts/DocWriter.cs
+++ Assets/Scripts/DocWriter.cs
@@ -32,37 +32,7 @@ void moveForward(Vector2 movement) {
 }
 void turnLeft() { rotation -= 90; }
 void turnRight() { rotation += 90; }
-void Execute(Command cmd) {
-    switch(cmd) {
-        case forward: moveForward(); break;
-        case left: turnLeft(); break;
-        case right: turnRight(); break;
-    }
-}
-void skip(int j) {
-    while(i > 0) {
-        commands.dequeue();
-        j--;
-    }
-}" : sys.solvedCode[4] ?
-            @"void Init() {
-    commands = new Queue<Command>();
-    position = startingPosition;
-    rotation = startingRotation;
-}
-void moveForward(Vector2 movement) {
-    position.x += movement.x;
-    position.y += movement.y;
-}
-void turnLeft() { rotation -= 90; }
-void turnRight() { rotation += 90; }
-void Execute(Command cmd) {
-    switch(cmd) {
-        case forward: moveForward(); break;
-        case left: turnLeft(); break;
-        case right: turnRight(); break;
-    }
-}" : sys.solvedCode[1] ?
+void skip(int j) { while(i > 0) { commands.dequeue(); j--; } }" : sys.solvedCode[1] ?
             @"void Init() {
     commands = new Queue<Command>();
     position = startingPosition;
@@ -87,7 +57,20 @@ void moveForward(Vector2 movement) {
         button = GameObject.Find("Doc2");
         button.GetComponent<Image>().color = sys.solvedCode[2] ? new Color32(158, 158, 158, 255) : new Color32(101, 101, 101, 255);
         button.GetComponent<Button>().enabled = sys.solvedCode[2];
-        button.transform.Find("DocCode").GetComponent<TextMeshProUGUI>().text = sys.solvedCode[2] ?
+        button.transform.Find("DocCode").GetComponent<TextMeshProUGUI>().text = sys.solvedCode[4] ?
+            @"bool checkCondition(Command cmd) {
+    nextMove = commands.dequeue();
+    if (nextMove == cmd) {
+        return true;
+    }
+}
+void Execute(Command cmd) {
+    switch(cmd) {
+        case forward: moveForward(); break;
+        case left: turnLeft(); break;
+        case right: turnRight(); break;
+    }
+}" : sys.solvedCode[2] ?
             @"bool checkCondition(Command cmd) {
     nextMove = commands.dequeue();
     if (nextMove == cmd) {
